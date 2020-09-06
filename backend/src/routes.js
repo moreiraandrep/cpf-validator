ac445e161622552ca03c23f053f7977efa9d1e8a
moreiraandrep <moreira.andre.p@gmail.com>
2020-09-06 17:47:18 -0300

    routes.js

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -12,19 +12,3 @@ routes.delete('/cep/:cep', CepController.destroy);
 routes.get('/search', SearchController.index);
 
 module.exports = routes;
-
-///instalação do MongoDB Compass Community 1:06:14
-
-/*Métodos HTTP: GET, POST, PUT, DELETE
-Tipos de parâmetros:
-   Query Params: mets. GET -> request.query (Filtros, Ordenação, Paginação, ...);
-   Route Params: mets. PUT, DELETE -> request.params (Identifica um registros para alterar ou deletar); 
-   Body: mets. POST, PUT -> request.body (Dados para criação ou alteração de um registro)
-
-routes.delete('/cep/:id', (request, response) => {
-   console.log(request.params);
-
-   return response.json({ message: "Deleto um CEP" });
-});
-
-*/
