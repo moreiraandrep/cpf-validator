d1b3a20a3b682c5002176edcf78d98293820016f
moreiraandrep <moreira.andre.p@gmail.com>
2020-10-12 10:06:04 -0300

    test regex

--- web/src/App.js
+++ web/src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Hello, World!</h1>
   );
 }
 
 export default App;
+
+
+const text = `
+- 58204-824
+- 69337-978
+- 69.938-863
+- 72874988
+`;
+const regex = /(\d{2}[.]?\d{3})[-]?(\d{3})/gm; //CEP
+           // /(\d{3}\.?\d{3}\.?\d{3}-?\d{2})/gm; //CPF
+console.log(text.match(regex));
+// [ '58204-824', '69337-978', '69.938-863', '72874988' ]
